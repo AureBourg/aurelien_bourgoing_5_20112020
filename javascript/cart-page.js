72277045e2b82d70f46e2329f29c597d9e734ef2
Aurelien <aurebourg@gmail.com>
2020-12-23 12:23:06 +0100

    mise en forme

--- javascript/cart-page.js
+++ javascript/cart-page.js
@@ -1,48 +1,96 @@
 //On récupère le tableau dans le storage
-const cartToDisplay = JSON.parse(localStorage.getItem('userCart'));
+const storageArray = JSON.parse(localStorage.getItem('userCart'));
+console.log(storageArray);
 
 //Afficher le chiffre du panier selon le nombre d'items dedans
 let cartNumber = document.getElementById('header_cart_number');
-cartNumber.innerHTML = cartToDisplay.length;
+cartNumber.innerHTML = storageArray.length;
 
 //Texte si le panier est vide
-if(cartToDisplay.length==0){
+if(storageArray.length==0){
     document.getElementById('main_cart_products').innerHTML+= "Votre panier est vide.";
 }else{
     $.get("http://localhost:3000/api/furniture/")
-        .done(function(productsLocalHost){
+        .done(function(localHostArray){
             let total = 0;
-            for(let objet of productsLocalHost){//On récupère les infos de chaque item du panier
-                for(let cartObjet of cartToDisplay){
-                    if(cartObjet.selectedProductId==objet._id){
+            for(let localHostObjet of localHostArray){//On récupère les infos de chaque item du panier
+                for(let cartObjet of storageArray){
+                    if(cartObjet.selectedProductId==localHostObjet._id){
                         document.getElementById('main_cart_products').innerHTML+= ""+
                         "<div class='col-12' id="+cartObjet.selectedProductId+">"+
                             "<div class='block_product col-12'>"+
-                                "<div class='block_product_price'>"+
-                                    objet.imageUrl+
+                                "<div class='block_product_img col-5'>"+
+                                    "<img src="+localHostObjet.imageUrl+" alt="+cartObjet.selectedProductId+"/>"+
                                 "</div>"+
-                                "<div class='block_product_name'>"+
-                                    objet.name+
+                                "<div class='block_product_infos col-4'>"+
+                                    "<div class='block_product_name'>"+
+                                        localHostObjet.name+
+                                    "</div>"+
+                                    "<div class='block_product_price'>"+
+                                        localHostObjet.price / 1000 +"0 €"+
+                                    "</div>"+
+                                    "<div class='block_product_varnish'>"+
+                                        "Vernis : "+cartObjet.selectedProductVarnish+
+                                    "</div>"+
+                                    "<div class='block_product_quantity'>"+
+                                        "<i id='quantity_minus' class='fas fa-minus'></i>"+
+                                        "<div id='quantity_number' class='block_product_quantity_number'>"+
+                                            cartObjet.selectedProductQty+
+                                        "</div>"+
+                                        "<i id='quantity_plus' class='fas fa-plus'></i>"+
+                                    "</div>"+
                                 "</div>"+
-                                "<div class='block_product_price'>"+
-                                    objet.price / 1000 +"0 €"+
-                                "</div>"+
-                                "<div class='block_product_varnish'>"+
-                                    cartObjet.selectedProductVarnish+
-                                "</div>"+
-                                "<div class='block_product_quantity'>"+
-                                    cartObjet.selectedProductQty+
-                                "</div>"+
-                                "<div id='product_subtotal' class='block_product_subtotal'>"+
-                                    (objet.price / 1000)*cartObjet.selectedProductQty+"0 €"+
+                                "<div class='block_product_total col-3'>"+
+                                    "<div id='product_subtotal' class='block_product_subtotal'>"+
+                                        ((localHostObjet.price / 1000)*cartObjet.selectedProductQty).toFixed(1)+"0 €"+
+                                    "</div>"+
+                                    "<div id='delete_item' class='block_product_delete'>"+
+                                        "<i class='fas fa-trash-alt'></i>"+
+                                    "</div>"+
                                 "</div>"+
                             "</div>"+
                         "</div>";
+
                         //Calcul prix total
-                        total = total + (objet.price / 1000)*cartObjet.selectedProductQty;
-                        if(cartToDisplay.length>1){
-                        document.getElementById('total_cart').innerHTML="Total ("+cartToDisplay.length+" articles) : "+total+"0 €";
-                        }else{document.getElementById('total_cart').innerHTML="Total ("+cartToDisplay.length+" article) : "+total+"0 €"};
+                        /*
+                        let intitotal=document.getid(total)
+                        inittotal=0
+                        inittotal = inittotal+document.getid(productsubtotal)*/
+                        total = total + (localHostObjet.price / 1000)*cartObjet.selectedProductQty;
+                        if(cartObjet.selectedProductQty>1){
+                        document.getElementById('total_cart').innerHTML="Total ("+cartObjet.selectedProductQty+" articles) : "+total.toFixed(1)+"0 €";
+                        }
+                        else{document.getElementById('total_cart').innerHTML="Total ("+cartObjet.selectedProductQty+" article) : "+total.toFixed(1)+"0 €"
+                        };
+
+                        //Bouton quantité + / -
+                        let btnMinus = document.getElementById('quantity_minus');
+                        btnMinus.onclick = function(){
+                            if(cartObjet.selectedProductQty>1){
+                                cartObjet.selectedProductQty--;
+                                localStorage.setItem('userCart', JSON.stringify(storageArray));
+                                window.location.reload();
+                                document.getElementById('quantity_number').innerHTML=cartObjet.selectedProductQty;
+                            }
+                        };
+                        let btnPlus = document.getElementById('quantity_plus');
+                        btnPlus.onclick = function(){
+                            if(cartObjet.selectedProductQty<9){
+                                cartObjet.selectedProductQty++;
+                                localStorage.setItem('userCart', JSON.stringify(storageArray));
+                                window.location.reload();
+                                document.getElementById('quantity_number').innerHTML=cartObjet.selectedProductQty;
+                            }
+                        };
+
+                        //Bouton supprimer article
+                        let btnDelete = document.getElementById('delete_item');
+                        btnDelete.onclick = function(){
+                            localStorage.removeItem[cartObjet];
+                            storageArray.splice(cartObjet, 1);
+                            localStorage.setItem('userCart', JSON.stringify(storageArray));
+                            window.location.reload();
+                        }
                     }
                 }
             }
--- javascript/js.js
+++ javascript/js.js
@@ -1,32 +1,3 @@
-/*let request = new XMLHttpRequest();
-request.onreadystatechange = function(){
-    if(this.readyState == XMLHttpRequest.DONE && this.status == 200){
-        let array = JSON.parse(this.responseText);
-        for(let i=0; i<array.length; i++){
-        document.getElementById('products').innerHTML+= ""+
-        "<div class='col-10 col-md-4'>"+
-            "<a href='product-page.html?id="+array[i]._id+"'>"+
-                "<div id='product' class='block_product'>"+
-                    "<div class='block_product_img'>"+
-                        "<img src='"+array[i].imageUrl+"' alt='"+array[i]._id+"'/>"+
-                    "</div>"+
-                    "<div class='block_product_name'>"+
-                        array[i].name+
-                    "</div>"+
-                    "<div class='block_product_price'>"+
-                        array[i].price / 1000 +" €"+
-                    "</div>"+
-                "</div>"+
-            "</a>"+
-        "</div>";
-        }
-        let cartNumber = document.getElementById('header_cart_number');
-        cartNumber.innerHTML = sessionStorage.length -1;
-    }
-}
-request.open("GET","http://localhost:3000/api/furniture");
-request.send();*/
-
 // Verifier si le panier est déja présent dans le storage
 if (localStorage.getItem("userCart")) {
 
@@ -48,14 +19,14 @@ $.get("http://localhost:3000/api/furniture/")
             document.getElementById('products').innerHTML+= ""+
             "<div class='col-10 col-md-4' id="+products[i]._id+">"+
                 "<a href='product-page.html?id="+products[i]._id+"'>"+
-                    "<div id='product' class='block_product'>"+
-                        "<div class='block_product_img'>"+
+                    "<div id='product' class='block_list_product'>"+
+                        "<div class='block_list_product_img'>"+
                             "<img src='"+products[i].imageUrl+"' alt='"+products[i]._id+"'/>"+
                         "</div>"+
-                        "<div class='block_product_name'>"+
+                        "<div class='block_list_product_name'>"+
                             products[i].name+
                         "</div>"+
-                        "<div class='block_product_price'>"+
+                        "<div class='block_list_product_price'>"+
                             products[i].price / 1000 +"0"+" €"+
                         "</div>"+
                     "</div>"+
